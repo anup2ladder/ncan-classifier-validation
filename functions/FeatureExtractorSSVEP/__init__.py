4f218b8a569748fb3c8f1086de7b71c31925f03a
Daniel Comaduran <38022865+danielcomaduran@users.noreply.github.com>
2024-02-12 15:59:32 -0700

    Added NCAN classifier and fix relative imports

---
+++ functions/FeatureExtractorSSVEP/__init__.py
@@ -0,0 +1,16 @@
+# from FeatureExtractorSSVEP.featureExtractor import FeatureExtractor
+
+# from FeatureExtractorSSVEP.featureExtractorTemplateMatching \
+#     import FeatureExtractorTemplateMatching
+    
+# from FeatureExtractorSSVEP.featureExtractorCCA import FeatureExtractorCCA
+
+# from FeatureExtractorSSVEP.featureExtractorMEC import FeatureExtractorMEC
+
+# from FeatureExtractorSSVEP.featureExtractorMSI import FeatureExtractorMSI
+
+from .featureExtractor import FeatureExtractor
+from .featureExtractorTemplateMatching import FeatureExtractorTemplateMatching
+from .featureExtractorCCA import FeatureExtractorCCA
+from .featureExtractorMEC import FeatureExtractorMEC
+from .featureExtractorMSI import FeatureExtractorMSI
